4f6debc8ce408012714b01a88b2abefdafd628e5
Giiordanni <jordani.alformiga@gmail.com>
2024-10-23 11:15:29 -0300

    Adicionando o logger para o secretario

--- backend/src/services/secretario.services.js
+++ backend/src/services/secretario.services.js
@@ -1,34 +1,57 @@
 import secRepositories from "../repositories/secretarios.repositories.js";
 import jwt from "jsonwebtoken";
 import middleware from "../middlewares/global.middlewares.js";
+import logger from "../logger/logger.mjs";
+import { log } from "winston";
 
 const createSec = async (body, role) => {
-
+  logger.info("Iniciando criação do Secretário");
   const {nome, email, senha, confirm_senha, cpf } = body;
 
   if ( !nome || !email || !cpf || !senha || !confirm_senha){
+    logger.error("Submeta todos os campos do registro")
     throw new Error("Submeta todos os campos do registro");
   }
 
-  if(!middleware.validarCPF(cpf)) throw new Error("CPF inválido");
+  if(!middleware.validarCPF(cpf)) {
+    logger.error("CPF inválido");
+    throw new Error("CPF inválido");
+  }
     
   const SecretarioExistente = await secRepositories.findByEmailSec(email);
-  if (SecretarioExistente) throw new Error("Usuário existente no bando de Dados");
+  if (SecretarioExistente) {
+    logger.error("Usuário existente no bando de Dados")
+    throw new Error("Usuário existente no bando de Dados")
+  };
 
-  if (senha !== confirm_senha) throw new Error("As senhas não coincidem");
-  if (senha.length < 6 ) throw new Error("A senha deve ter no mínimo 6 caracteres");
-  if (senha.length > 20 ) throw new Error("A senha deve ter no máximo 20 caracteres");
+  if (senha !== confirm_senha) {
+    logger.error("As senhas não coincidem");
+    throw new Error("As senhas não coincidem");
+  }
+  if (senha.length < 6) {
+    logger.error("A senha deve ter no mínimo 6 caracteres");
+    throw new Error("A senha deve ter no mínimo 6 caracteres");
+  }
+  if (senha.length > 20) {
+    logger.error("A senha deve ter no máximo 20 caracteres");
+    throw new Error("A senha deve ter no máximo 20 caracteres");
+  }
 
   try {
     await middleware.validarEmail(email);
   } catch (error) {
+    logger.error(`Erro na validação do email: ${error.message}`);
     throw new Error(`Erro na validação do email: ${error.message}`);
   }
 
   const user_secretario = await secRepositories.createSec(body);
-  if (!user_secretario) throw new Error("Erro ao criar usuário");
+  if (!user_secretario) {
+    logger.error("Erro ao criar usuário");
+    throw new Error("Erro ao criar usuário")
+  };
 
   const token = genarateToken(user_secretario.id, role);
+  logger.info("Usuário criado com sucesso");
   
   return {
     user: {
@@ -42,22 +65,34 @@ const createSec = async (body, role) => {
 };
 
 const findAllSec = async () => {
+  logger.info("Buscando todos os Secretários");
     const users = await secRepositories.findAllSec();
-    if (users.length === 0) throw new Error ("Não há usuaários cadastrados!");
+    if (users.length === 0) {
+      logger.error("Não há usuaários cadastrados!");
+      throw new Error ("Não há usuaários cadastrados!")
+    };
+    logger.info("Secretários encontrados com sucesso");
     return users;
-    
 };
 
 const findOne = async (id) =>{
+  logger.info("Buscando Secretário por ID");
   const user = await secRepositories.findById(id);
+  if(!user){
+    logger.error("Usuário não encontrado");
+    throw new Error("Usuário não encontrado");
+  }
+  logger.info("Usuário encontrado com sucesso");
   return user;
-}
+};
 
 const loginSec = (email) => {
+  logger.info(`Buscando usuário para login com email: ${email}`);
   return secRepositories.findByEmailSec(email).select('+senha');
 };
 
 const genarateToken = (user, role) => {
+  logger.info("Gerando o token de autenticação");
   return jwt.sign(
     { _id: user._id, role: role}, 
     process.env.SECRETJWT, { 
@@ -66,13 +101,21 @@ const genarateToken = (user, role) => {
 };
 
 const deleteSec = async (id) => {
+  logger.info("Deletando secretário");
   const user = await secRepositories.findById(id);
-  if (!user) throw new Error("Usuaário não encontrado");
-  return secRepositories.deleteSec(id);
+  if (!user) {
+    logger.info("Usuaário não encontrado");
+    throw new Error("Usuaário não encontrado");
+  }
+  await secRepositories.deleteSec(id);
+  logger.info("Usuário deletado com sucesso");
 }
 
 const updateSec = async (email, update) => {
-  return secRepositories.patchDataSec(email, update);
+  logger.info(`Atualizando usuário com email: ${email}`);
+  const result =  secRepositories.patchDataSec(email, update);
+  logger.info("Secretário atualizado com sucesso");
+  return result;
 };
 
 export default {
@@ -81,5 +124,6 @@ export default {
   findOne,
   loginSec,
   genarateToken,
-  deleteSec
+  deleteSec, 
+  updateSec
 };
